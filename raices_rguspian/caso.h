18292a4b27753e49c758d3d0efb7f34c5394e8fd
Rodrigo A Guspian <rodalejo8@gmail.com>
2024-08-26 02:23:44 -0500

    separacion

--- raices_rguspian/caso.h
+++ raices_rguspian/caso.h
@@ -7,10 +7,12 @@
 
 #ifndef CASO_H
 #define CASO_H
+ 
 void caso_1_tvi();
 void caso_1_biseccion();
 void caso_1_regla_falsa();
 void caso_1_newton_rapshon();
 void caso_2_newton_rapshon();
 void caso_3_secante();
+
 #endif
